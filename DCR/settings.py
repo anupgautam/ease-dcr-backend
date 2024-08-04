a0fecded064072ceaf562623f3aac5abc680d2c1
anupgautam60 <anupgautam60@gmail.com>
2024-08-04 21:33:40 +0545

    database config

--- DCR/settings.py
+++ DCR/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ["api.easesfa.com", ".ngrok.io", "localhost", "*"]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "daphne",
     "channels",
@@ -122,28 +121,28 @@ ASGI_APPLICATION = "DCR.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dcr',
-#         'USER': 'postgres',
-#         'HOST': 'localhost',
-#         'PASSWORD': 'password',
-#         'PORT': '5432',
-
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "NewDCR1",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PASSWORD": "czvxynmch",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcr',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': 'password',
+        'PORT': '5432',
+
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "NewDCR1",
+#         "USER": "postgres",
+#         "HOST": "localhost",
+#         "PASSWORD": "czvxynmch",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
