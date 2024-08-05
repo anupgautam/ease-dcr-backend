461d92cb763fa5d75bbbb719f273b52466790519
Aayush Gautam <aayushgautam201@gmail.com>
2024-08-05 22:04:00 +0530

    [base urls updated]

--- DCR/settings.py
+++ DCR/settings.py
@@ -121,28 +121,28 @@ ASGI_APPLICATION = "DCR.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dcr',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PASSWORD': 'password',
-        'PORT': '5432',
-
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "NewDCR1",
-#         "USER": "postgres",
-#         "HOST": "localhost",
-#         "PASSWORD": "czvxynmch",
-#         "PORT": "5432",
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dcr',
+#         'USER': 'postgres',
+#         'HOST': 'localhost',
+#         'PASSWORD': 'password',
+#         'PORT': '5432',
+
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dcr",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PASSWORD": "password",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
