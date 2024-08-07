8d392f917fed2d9c028cff8a08a1331202af1155
anupgautam60 <anupgautam60@gmail.com>
2024-08-07 09:56:41 +0545

    localhost host name

--- DCR/settings.py
+++ DCR/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "dcr",
         "USER": "postgres",
-        "HOST": "159.65.145.104",
+        "HOST": "localhost",
         "PASSWORD": "password",
         "PORT": "5432",
     }
