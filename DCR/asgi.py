3149d51072a8607d62c2660b4d78ac76678b61ee
anupgautam60 <anupgautam60@gmail.com>
2024-08-11 18:51:45 +0545

    chat routing order changes

--- DCR/asgi.py
+++ DCR/asgi.py
@@ -12,10 +12,10 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DCR.settings')
 django.setup()
+import chat.routing
 
 # Debug: Verify if the apps are ready
 if django.apps.apps_ready:
