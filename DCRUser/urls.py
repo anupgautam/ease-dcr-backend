feb062c0a9985097fec66437184ce367d53da784
anupgautam60 <anupgautam60@gmail.com>
2024-08-16 20:01:53 +0545

    make user present conflict error

--- DCRUser/urls.py
+++ DCRUser/urls.py
@@ -11,7 +11,8 @@ from DCRUser.views import (UserViewset,
                         get_all_the_lower_level_users_from_company_user_role_id,
                         get_user_hierarchy,
                         get_admin_user_id,
-                        make_user_present)
+                        # make_user_present
+                        )
 from django.urls import path
 
 
@@ -34,6 +35,6 @@ urlpatterns = [
     path('get-all-lower-level-users-from-company-user-role-id/',get_all_the_lower_level_users_from_company_user_role_id),
     path('get-user-hierarchy/',get_user_hierarchy),
     path('get-admin-id/',get_admin_user_id),
-    path('make-user-present/',make_user_present),
+    # path('make-user-present/',make_user_present),
 ]
 urlpatterns += router.urls
