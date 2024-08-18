3b514dfaf5d848651ed87e9150cee8a68f252bfd
anupgautam60 <anupgautam60@gmail.com>
2024-08-18 06:54:29 +0545

    mpo serializers changes

--- Expenses/serializers.py
+++ Expenses/serializers.py
@@ -1,10 +1,10 @@
-from datetime import date
 from nepali_date_converter import nepali_today
-
+from datetime import datetime, date
 from rest_framework import serializers
 from Expenses.models import *
 from Company.serializers import CompanyAreaSerializers
 from DCRUser.serializers import CompanyUserRoleSerializers
+from bsdate.convertor import BSDateConverter 
 
 
 class ExpensesTypeSerializers(serializers.ModelSerializer):
@@ -52,6 +52,7 @@ class LeaveApplicationSerializers(serializers.ModelSerializer):
 
 class MpoWiseLeaveApplicationSerializers(serializers.ModelSerializer):
     application_id = LeaveApplicationSerializers()
+
     class Meta:
         model = MpoWiseLeaveApplication
         fields = '__all__'
@@ -68,7 +69,6 @@ class MpoWiseLeaveApplicationSerializers(serializers.ModelSerializer):
         mpo_wise_leave_application.save()
         return mpo_wise_leave_application
     
-
     def update(self, instance, validated_data):
         application_data = validated_data['application_id']
         leave_application_instance = LeaveApplication.objects.get(id=instance.application_id.id)
@@ -76,7 +76,6 @@ class MpoWiseLeaveApplicationSerializers(serializers.ModelSerializer):
         leave_application_instance.leave_cause = application_data['leave_cause']
         leave_application_instance.leave_from = application_data['leave_from']
         leave_application_instance.leave_to = application_data['leave_to']
-        # leave_application_instance.submit_to = application_data['submit_to']
         leave_application_instance.leave_status = application_data['leave_status']
         leave_application_instance.is_submitted = application_data['is_submitted']
         leave_application_instance.is_approved = application_data['is_approved']
@@ -90,23 +89,35 @@ class MpoWiseLeaveApplicationSerializers(serializers.ModelSerializer):
     
     def to_representation(self, instance):
         response = super().to_representation(instance) 
-        response['mpo_name'] = CompanyUserRoleSerializers(
-            instance.mpo_name
-        ).data
+        response['mpo_name'] = CompanyUserRoleSerializers(instance.mpo_name).data
         return response
     
     def validate_application_id(self, attrs):
+        converter = BSDateConverter()
+        
+        leave_from_bs = attrs['leave_from']
+        leave_to_bs = attrs['leave_to']
+        
+        # Convert BS dates to AD dates
+        leave_from_ad = datetime.strptime(converter.convert_bs_to_ad(leave_from_bs), '%Y-%m-%d').date()
+        leave_to_ad = datetime.strptime(converter.convert_bs_to_ad(leave_to_bs), '%Y-%m-%d').date()
+        
+        # Get today's date in AD (Gregorian)
+        nepali_today_ad = date.today()
+
         if not attrs['leave_cause']:
-            raise serializers.ValidationError('Leave cause is mandatory')
-        if not attrs['leave_from']:
-            raise serializers.ValidationError("leave from date is mandatory")
-        if not attrs['leave_to']:
-            raise serializers.ValidationError("Leave to date is mandatory.")
-        if (
-            attrs['leave_from'] < date(nepali_today.year, nepali_today.month, nepali_today.day) 
-            or attrs['leave_to'] < date(nepali_today.year, nepali_today.month, nepali_today.day)):
-            raise serializers.ValidationError('Invalid leave date')
+            raise serializers.ValidationError('Leave cause is mandatory.')
+        if not leave_from_bs:
+            raise serializers.ValidationError('Leave from date is mandatory.')
+        if not leave_to_bs:
+            raise serializers.ValidationError('Leave to date is mandatory.')
+
+        # Check if the leave dates are before today
+        if leave_from_ad < nepali_today_ad or leave_to_ad < nepali_today_ad:
+            raise serializers.ValidationError('Invalid leave date: leave dates cannot be before today.')
+
         return attrs
+
 
 class TargetSerializers(serializers.ModelSerializer):
     year = serializers.CharField(
