a097b298d725cc2dad4a820d62345954ab58394e
unknown <ankitdawadi@gmail.com>
2024-08-31 22:33:05 +0545

    fix: added count condtion for company holiday area

--- Company/views.py
+++ Company/views.py
@@ -233,9 +233,9 @@ class CompanyHolidayAreaViewset(viewsets.ModelViewSet):
                 company_area=CompanyArea.objects.get(id=comp_area),
                 holiday_type=CompanyHoliday.objects.get(id=holiday_type))
             for comp_area in company_area
-            if not CompanyHolidayArea.objects.get(
+            if CompanyHolidayArea.objects.filter(
                 company_area__id=comp_area,
-                holiday_type__id=holiday_type)
+                holiday_type__id=holiday_type).count() == 0
         ]
         CompanyHolidayArea.objects.bulk_create(company_holiday_area)
         return Response(
