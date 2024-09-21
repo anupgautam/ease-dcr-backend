b134147a7df33abe56188a6847b5d2fad909e5f7
anupgautam60 <anupgautam60@gmail.com>
2024-09-21 21:50:08 +0545

    notification offed

--- Mpo/utils.py
+++ Mpo/utils.py
@@ -43,9 +43,8 @@ def get_year_month_from_date(selected_date):
 
 def mpo_data_transmission(request):
     # company_name = request.data.get('company_name')
+    print('hello',request.data)
     dates = request.data.get('dates')
-    print('yes ma xa nee')
-    print('dates',request.data.get('dates'))
     approved_by = request.data.get('approved_by')
     # year_month = get_year_month_from_date(dates[0])
     # instance = CompanyUserRole.objects.get(id=request.data.get('mpo_name'))
